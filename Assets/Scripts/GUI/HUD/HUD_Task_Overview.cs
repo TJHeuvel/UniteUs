6bfe51f1df509399cc3cbc462ad77f5b08aee87a
Tijmen van den Heuvel <info@tjheuvel.net>
2020-12-13 17:36:47 +0100

    Implement task overview ui

---
+++ Assets/Scripts/GUI/HUD/HUD_Task_Overview.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+class HUD_Task_Overview : MonoBehaviour 
+{
+    [SerializeField] private Slider sldrCompletedTasks;
+    void OnEnable()
+    {
+        TaskManager.Instance.TotalTaskCount.OnValueChanged += onTasksChanged;
+        TaskManager.Instance.CompletedTaskCount.OnValueChanged += onTasksChanged;
+    }
+    void OnDisable()
+    {
+        TaskManager.Instance.TotalTaskCount.OnValueChanged -= onTasksChanged;
+        TaskManager.Instance.CompletedTaskCount.OnValueChanged -= onTasksChanged;
+    }
+
+    private void onTasksChanged(int previousValue, int newValue)
+    {
+        sldrCompletedTasks.maxValue = TaskManager.Instance.TotalTaskCount.Value;
+        sldrCompletedTasks.value = TaskManager.Instance.CompletedTaskCount.Value;
+    }
+}
